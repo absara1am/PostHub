890f5b411da083d7c4ba123037fc493d7e89d7d5
absar-a1am <workwithabsar@gmail.com>
2024-03-21 15:12:43 +0530

    feat: Implement backend for blogging application
    - Created User entity to represent blog users
    - Implemented UserDto for data transfer objects
    - Added UserRepository for CRUD operations on User entities
    - Defined UserService interface with methods for user management
    
    This commit establishes the foundational backend structure for the blogging application, including entity mapping, data transfer objects, repository setup, and service layer definition. The architecture follows industry best practices and aims for modularity and scalability.

---
+++ blog-app-apis/src/main/java/com/absara1am/blog/repositories/UserRepo.java
@@ -0,0 +1,10 @@
+package com.absara1am.blog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.absara1am.blog.entities.User;
+
+public interface UserRepo extends JpaRepository<User, Integer> {
+	
+
+}
---
+++ blog-app-apis/src/test/java/com/absara1am/blog/BlogAppApisApplicationTests.java
@@ -0,0 +1,21 @@
+package com.absara1am.blog;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+class BlogAppApisApplicationTests {
+	
+
+
+	@Test
+	void contextLoads() {
+	}
+
+
+	
+	
+
+}
