0cf083699979054b0eb403842e4e55e5a6870308
absar-a1am <workwithabsar@gmail.com>
2024-03-21 15:29:16 +0530

    feat: Add UserController, UserServiceImpl, and ResourceNotFoundException
    - Created UserController to handle user-related API endpoints
    - Implemented UserServiceImpl to provide CRUD operations for users
    - Added ResourceNotFoundException for handling resource not found errors
    
    These changes enhance the backend of the blogging application by introducing controllers and services to manage users. Additionally, a custom exception class is included to handle resource not found scenarios efficiently.

---
+++ blog-app-apis/src/main/java/com/absara1am/blog/services/impl/UserServiceImpl.java
@@ -0,0 +1,109 @@
+package com.absara1am.blog.services.impl;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.absara1am.blog.entities.User;
+import com.absara1am.blog.payloads.UserDto;
+import com.absara1am.blog.repositories.UserRepo;
+import com.absara1am.blog.services.UserService;
+import com.absara1am.blog.exceptions.ResourceNotFoundException;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepo userRepo;
+
+	@Override
+	public UserDto createUser(UserDto userDto) {
+		
+		User user = this.dtoToUser(userDto);
+		User savedUser = this.userRepo.save(user);
+		
+		return this.userToDto(savedUser);
+	}
+
+	@Override
+	public UserDto updateUser(UserDto userDto, Integer userId) {
+		
+		//get
+		User user = this.userRepo.findById(userId).
+				orElseThrow( () -> new ResourceNotFoundException("User", "Id", userId));
+		
+		//set
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setAbout(userDto.getAbout());
+		user.setPassword(userDto.getPassword());
+		
+		//update
+		User updatedUser = this.userRepo.save(user);
+		UserDto userDto1 = this.userToDto(updatedUser);
+		
+		return userDto1;
+	}
+
+	@Override
+	public UserDto getUserById(Integer userId) {
+		//get
+		User user = this.userRepo.findById(userId).
+				orElseThrow( () -> new ResourceNotFoundException("User", "Id", userId));
+		
+		return this.userToDto(user);
+	}
+
+	@Override
+	public List<UserDto> getAllUsers() {
+		
+		List<User> users = this.userRepo.findAll();
+		
+		List<UserDto> userDtos =  users.stream().map(user -> this.userToDto(user)).collect(Collectors.toList());
+		
+		return userDtos;
+	}
+
+	@Override
+	public void deleteUser(Integer userId) {
+		
+		User user = this.userRepo.findById(userId).
+				orElseThrow( () -> new ResourceNotFoundException("User", "Id", userId));
+		 
+		this.userRepo.delete(user);
+
+	}
+	
+	private User dtoToUser(UserDto userDto) {
+		
+		User user = new User();
+		
+		user.setId(userDto.getId());
+		user.setName(userDto.getName());
+		user.setEmail(userDto.getEmail());
+		user.setAbout(userDto.getAbout());
+		user.setPassword(userDto.getPassword());
+		
+		return user;
+			
+	}
+	
+	private UserDto userToDto(User user) {
+		
+		UserDto userDto = new UserDto();
+		
+		userDto.setId(user.getId());
+		userDto.setName(user.getName());
+		userDto.setEmail(user.getEmail());
+		userDto.setAbout(user.getAbout());
+		userDto.setPassword(user.getPassword());
+		
+		return userDto;
+			
+	}
+
+	
+	
+}
--- blog-app-apis/src/test/java/com/absara1am/blog/BlogAppApisApplicationTests.java
+++ blog-app-apis/src/test/java/com/absara1am/blog/BlogAppApisApplicationTests.java
@@ -1,20 +1,30 @@
 package com.absara1am.blog;
 
 import org.junit.jupiter.api.Test;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.absara1am.blog.repositories.UserRepo;
 
 @SpringBootTest
 class BlogAppApisApplicationTests {
 	
-
+	@Autowired
+	private UserRepo userRepo;
 
 	@Test
 	void contextLoads() {
 	}
-
-
+	
+	@Test
+	public void repoTest() {
+		
+		String className = this.userRepo.getClass().getName();
+		String packageName = this.userRepo.getClass().getPackageName();
+		System.out.println(className);
+		System.out.println(packageName);
+		
+	}
 	
 	
 
