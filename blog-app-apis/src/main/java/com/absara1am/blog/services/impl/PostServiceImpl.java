babc7e65762278c5536b64ce0bcf93aab80d8c8b
absar-a1am <workwithabsar@gmail.com>
2024-06-07 15:31:31 +0530

    feat: Enhance Post API with pagination and sorting
    - Introduced a pagination response structure in PostResponse with pageSize, pageNumber, totalElements, totalPages, lastPage, and content. The getAllPosts() method now returns PostResponse instead of a list.
    - Added sorting functionality in the getAllPosts method in PostService and PostServiceImpl.
    - Updated PostController to handle sortBy and sortDir parameters for sorting posts by specified fields in ascending or descending order.

--- blog-app-apis/src/main/java/com/absara1am/blog/services/impl/PostServiceImpl.java
+++ blog-app-apis/src/main/java/com/absara1am/blog/services/impl/PostServiceImpl.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
 import com.absara1am.blog.entities.Category;
@@ -83,9 +84,16 @@ public class PostServiceImpl implements PostService {
 	}
 
 	@Override
-	public PostResponse getAllPosts(Integer pageNumber, Integer pageSize) {
+	public PostResponse getAllPosts(Integer pageNumber, Integer pageSize, String sortBy, String sortDir) {
 
-		Pageable p = PageRequest.of(pageNumber, pageSize);
+		Sort sort = null;
+		if (sortDir.equalsIgnoreCase("asc")) {
+			sort = Sort.by(sortBy).ascending();
+		} else if (sortDir.equalsIgnoreCase("desc")) {
+			sort = Sort.by(sortBy).descending();
+		}
+
+		Pageable p = PageRequest.of(pageNumber, pageSize, sort);
 
 		Page<Post> pagePost = this.postRepo.findAll(p);
 		List<Post> allPosts = pagePost.getContent();
